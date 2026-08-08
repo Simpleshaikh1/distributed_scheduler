6b5304552817ceca76c499e280e03677d9f36e10
Toyyib <hamzatbolaji648@yahoo.com>
2026-08-08 10:12:13 +0100

    fixes

--- internal/worker/pool.go
+++ internal/worker/pool.go
@@ -3,17 +3,30 @@ package worker
 import (
 	"context"
 	"github.com/Simpleshaikh1/distributed_scheduler/internal/job"
+	"sync"
 )
 
 // Now I make the pool own the workers and the queue
 type Pool struct {
-	workers []*Worker
-
 	jobs chan job.Execution
+
+	workers int
+
+	executor Executor
+
+	wg sync.WaitGroup
 }
 
 func (p *Pool) Start(ctx context.Context) {
-	for _, worker := range p.workers {
-		go worker.Run(ctx)
+	for i := 0; i < p.workers; i++ {
+		p.wg.Add(1)
+
+		worker := Worker{
+			id:       1,
+			jobs:     p.jobs,
+			executor: p.executor,
+		}
+
+		go worker.Run(ctx, &p.wg)
 	}
 }
